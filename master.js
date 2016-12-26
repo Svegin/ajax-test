47d3b41c9facfc9a2f8b5a147ecd85b507390bf5
Svegin <svegin@Svegins-Mac-mini.local>
2016-12-25 22:10:57 -0600

    finished search

--- master.js
+++ master.js
@@ -6,8 +6,10 @@ fetch(endpoint)
   .then(blob => blob.json())
   .then(data => cities.push(...data));
 
-  function findMatches(wordMatch, cities) {
+  function findMatches(wordToMatch, cities) {
       return cities.filter(place => {
         //here we need to figure out if the city or statematches what was searched
-      })
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+      });
   }
