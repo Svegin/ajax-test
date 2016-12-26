ffa47ce757b735033d545aedb54fdfd458c7db97
Svegin <svegin@Svegins-Mac-mini.local>
2016-12-25 21:53:34 -0600

    change search

--- master.js
+++ master.js
@@ -1,1 +1,13 @@
+// const endpoint = 'cities.json';
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities = [];
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+
+  function findMatches(wordMatch, cities) {
+      return cities.filter(place => {
+        //here we need to figure out if the city or statematches what was searched
+      })
+  }
